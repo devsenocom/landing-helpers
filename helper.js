35a44621d31ce7a7e650e60cf078284a70ad6492
R K <ceo@senocom.com>
2026-08-18 18:15:00 +0400

    add iOS link to LG

--- helper.js
+++ helper.js
@@ -61,6 +61,7 @@ const AppConfig = (() => {
     lust_goddess: {
       game_url: "https://lustgoddess.com/play",
       game_url_android: "https://lustgoddess.com/play",
+      game_url_ios: "https://onside.io/lust-goddess?variant=adult",
       analytics_url:
         "https://ingest.lu-analytics.com/preland_stats/adult_lyssa/visits",
       title: "Lust Goddess",
@@ -78,6 +79,7 @@ const AppConfig = (() => {
     lust_goddess_mirror: {
       game_url: "https://2025lustgoddess.com/play",
       game_url_android: "https://2025lustgoddess.com/play",
+      game_url_ios: "https://onside.io/lust-goddess?variant=adult",
       analytics_url:
         "https://ingest.lu-analytics.com/preland_stats/adult_lyssa/visits",
       title: "Lust Goddess",
